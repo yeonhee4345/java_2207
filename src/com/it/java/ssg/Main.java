b8da9dc849f5d521c9962c22060f4f22ddeab2a3
yeonhee4345 <yeonhee4345@gmail.com>
2022-07-16 13:54:55 +0900

    게시물 상세페이지 기능 구현, 날짜는 처리 못함

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -49,6 +49,31 @@ public class Main {
 					System.out.printf("%d    |    %s\n" , article.id, article.title);
 				}
 			}
+			
+			else if(command.startsWith("article detail")) {
+				String[] commandbits = command.split(" ");
+				int id = Integer.parseInt(commandbits[2]); // "2" => 2
+				
+				Article foundArticle = null;
+				
+				for(int i =0; i <articles.size(); i++) {
+					Article article = articles.get(i);
+					if(article.id == id) {
+						
+						foundArticle = article;
+						break;
+					}
+				}
+				if(foundArticle == null) {					
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);	
+					continue;
+				}
+				System.out.printf("번호 : %d\n",foundArticle.id);
+				System.out.printf("날짜 : 2022-07-16 12:41:15\n");
+				System.out.printf("제목 : %s\n",foundArticle.title);
+				System.out.printf("내용 : %s\n",foundArticle.body);
+				
+			}
 			else {
 				System.out.printf("%s는 존재하지 않는 명령어입니다.\n",command);
 			}

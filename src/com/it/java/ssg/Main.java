4074536fa25fc678933607675eb2f20ea1e394d3
yeonhee4345 <yeonhee4345@gmail.com>
2022-07-17 13:42:10 +0900

    게시물 수정기능 추가

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -75,6 +75,33 @@ public class Main {
 				System.out.printf("내용 : %s\n",foundArticle.body);	
 			}
 			
+			else if(command.startsWith("article modify")) {
+				String[] commandbits = command.split(" ");
+				int id = Integer.parseInt(commandbits[2]); // "2" => 2
+				
+				Article foundArticle = null;
+				
+				for(int i =0; i <articles.size(); i++) {
+					Article article = articles.get(i);
+					if(article.id == id) {
+						
+						foundArticle = article;
+						break;
+					}
+				}
+				if(foundArticle == null) {					
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);	
+					continue;
+				}
+				System.out.printf("제목 : ");
+				String title = scan.nextLine();
+				System.out.printf("내용 : ");
+				String body = scan.nextLine();
+				foundArticle.title = title;
+				foundArticle.body = body;
+				System.out.printf("%d번 게시물이 수정되었습니다.\n",id);
+			}
+			
 			else if (command.startsWith("article delete")) {
 				String[] commandbits = command.split(" ");
 				int id = Integer.parseInt(commandbits[2]);
